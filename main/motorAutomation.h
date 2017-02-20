f7904235b546317d6ddac53985024eaeeffb34c8
balintmaci <balintmaci@gmail.com>
2017-02-20 20:02:51 +0100

    fix, debug mode

--- main/motorAutomation.h
+++ main/motorAutomation.h
@@ -1,7 +1,7 @@
 #define multiplier 1.68
 #define maxPower 100
+#define P 0.01
 
-#include "motorControl.h"
 #include "encoderReader.h"
 
 unsigned long lastTime = 0;
@@ -37,18 +37,29 @@ double getSpeed1(double Time)
 //Sets speed in mm/sec
 void SetSpeed(double goSpeed0, double goSpeed1)
 {
-  double _time = micros() - lastTime;
+  double _time = (micros() - lastTime) / 1000000.0;
   double speed0 = getSpeed0(_time);
   double speed1 = getSpeed1(_time);
   lastTime = micros();
-  
-  double error0 = goSpeed0-speed0;
-  double P=0.1;
-  speed0=speed0+error0*P;
-  
-  SetMotorPower(speed0, speed1);
+#ifdef DEBUG
+  if (!overflower);
+  Serial.print("Left: ");
+  Serial.print(speed0);
+  Serial.print(" Right: ");
+  Serial.println(speed1);
+#endif
+  double error0 = goSpeed0 - speed0;
+  speed0 = s0 + error0 * P;
 
-  
+  SetMotorPower(speed0, 0);
+
+#ifdef DEBUG
+  if (!overflower);
+  Serial.print("Level: ");
+  Serial.println(speed0);
+#endif
+  overflower++;
+
 }
 
 
