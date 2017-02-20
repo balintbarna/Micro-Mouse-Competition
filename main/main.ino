01ccead5b2331af01a265450bbb57b36574718bf
balintmaci <balintmaci@gmail.com>
2017-02-20 18:03:36 +0100

    Sebességmérés hozzáadva

--- main/main.ino
+++ main/main.ino
@@ -1,16 +1,38 @@
-#include "motorControl.h"
+#include "motorAutomation.h"
+byte overflower = 0;
 
 void setup() {
-  Serial.begin(9600);
-  setupMotors();
+  //Initialize Serial Comm
+  Serial.begin(115200);
+  //Initialize Motor Automation
+  SetupMororAutomation();
 }
 
 void loop() {
+  if (overflower == 0)
+    displaySpeeds();
+  serialToMotors();
+  overflower++;
+  delay(10);
+}
+
+void serialToMotors() {
   if (Serial.available()) {
-    delay(50);
+    delay(10);
     int outputValue = Serial.parseInt();
-    setMotorPower(outputValue, outputValue);
+    SetMotorPower(outputValue, outputValue);
     Serial.println(outputValue);
   }
-  delay(50);
 }
+
+void displaySpeeds() {
+  double _time = (micros() - lastTime) / 1000000.0;
+  double speed0 = getSpeed0(_time);
+  double speed1 = getSpeed1(_time);
+  lastTime = micros();
+  Serial.print("Left: ");
+  Serial.print(speed0);
+  Serial.print(" Right: ");
+  Serial.println(speed1);
+}
+
