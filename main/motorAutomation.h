f8a39b8f2af1b1c358b5874ae4e282a0a8839761
balintmaci <balintmaci@gmail.com>
2017-03-07 16:33:10 +0100

    PI szabályzás sebességre

--- main/motorAutomation.h
+++ main/motorAutomation.h
@@ -1,24 +1,91 @@
+//PID controllers
+#define Ptag 0.1
+#define Dtag 0.1
+#define Itag 0.1
 
-#define Ptag 3
-#define Dtag 0
-#define Itag 0
-
+//timer interval (microsec)
 #define myinterval 5000
-#define accuracy 2
 
+//position control accuracy
+#define accuracy 1
+
+//constants for recursive filter
+#define newPart 0.1
+const double oldPart = 1 - newPart;
+
+//maximum acceleration (speed change in a cycle)
+#define maxAccelC 0.00001
+const double maxAccel = myinterval * maxAccelC;
+
+//Tárolók
+//időmérő
 //elapsedMicros elapsedTime;
-//double aggrSpeedA = 0;
-//double aggrSpeedB = 0;
+//szűrt sebesség hiba
+volatile double aggrSpeedA = 0;
+volatile double aggrSpeedB = 0;
+//szűrt pozívió hiba derivált
 volatile double errDiffAggrA = 0;
 volatile double errDiffAggrB = 0;
+//régi hiba
 volatile double errOldA = 0;
 volatile double errOldB = 0;
+//integráló tag
 volatile double errSumA = 0;
 volatile double errSumB = 0;
+//régi setspeed
+volatile double pastSetSpeedA = 0;
+volatile double pastSetSpeedB = 0;
+//timer
 IntervalTimer myTimer;
 
 #include "encoderReader.h"
 
+//Speed control PI
+void SetMotorSpeed(double setSpeedA, double setSpeedB)
+{
+  //Maximum gyorsulás beállítása
+  //if (abs(setSpeedA - pastSetSpeedA) > maxAccel) setSpeedA = pastSetSpeedA + sign(setSpeedA - pastSetSpeedA) * maxAccel;
+  //if (abs(setSpeedB - pastSetSpeedB) > maxAccel) setSpeedB = pastSetSpeedB + sign(setSpeedB - pastSetSpeedB) * maxAccel;
+
+  //Sebesség számítása + rekurzív szűrés sebességre
+  aggrSpeedA = oldPart * aggrSpeedA + newPart * (distanceA - pastA);
+  aggrSpeedB = oldPart * aggrSpeedB + newPart * (distanceB - pastB);
+
+  //0 közeli hiba kiiktatása
+  if (abs(aggrSpeedA) < 0.00001)
+  {
+    aggrSpeedA = 0;
+  }
+  if (abs(aggrSpeedB) < 0.00001)
+  {
+    aggrSpeedB = 0;
+  }
+
+  //Sebesség hiba számítása
+  double errorA = setSpeedA - aggrSpeedA;
+  double errorB = setSpeedB - aggrSpeedB;
+
+  //Tárolóba pozíció mentés
+  pastA = distanceA;
+  pastB = distanceB;
+
+  //Integráló tag növelése
+  errSumA += errorA;
+  errSumB += errorB;
+
+  //Tárolóba setSpeet metése
+  pastSetSpeedA = setSpeedA;
+  pastSetSpeedB = setSpeedB;
+
+  //double opVA = sA + PtagSpeed * errorA + Itag * errSumA;
+  //double opVB = sB + PtagSpeed * errorB + Itag * errSumB;
+  double opVA = Ptag * errorA + Itag * errSumA;
+  double opVB = Ptag * errorB + Itag * errSumB;
+
+  SetMotorPower(opVA, opVB);
+}
+
+//Position control (incremental) PD
 void SetPos(double spA, double spB)
 {
   double errorA = spA - distanceA;
@@ -35,23 +102,26 @@ void SetPos(double spA, double spB)
     errSumB = 0;
     errDiffAggrB = 0;
   }
-  errDiffAggrA = 0.99 * errDiffAggrA + 0.01 * (errorA - errOldA);
-  errDiffAggrB = 0.99 * errDiffAggrB + 0.01 * (errorB - errOldB);
+  errDiffAggrA = 0.9 * errDiffAggrA + 0.1 * (errorA - errOldA);
+  errDiffAggrB = 0.9 * errDiffAggrB + 0.1 * (errorB - errOldB);
   errSumA = 0.9 * errSumA + errorA;
   errSumB = 0.9 * errSumB + errorB;
 
-  double opVA = Ptag * errorA + Dtag * errDiffAggrA + Itag * errSumA;
-  double opVB = Ptag * errorB + Dtag * errDiffAggrB + Itag * errSumB;
+  double opVA = Ptag * errorA + Dtag * errDiffAggrA;
+  double opVB = Ptag * errorB + Dtag * errDiffAggrB;
 
   errOldA = errorA;
   errOldB = errorB;
 
-  //SetMotorPower(opVA, opVB);
+  SetMotorSpeed(opVA, opVB);
 }
 
+//Functions gets called by timer ticks
 void automInterrupt()
 {
-  SetPos(setPoint, setPoint);
+  //SetPos(setPoint, setPoint);
+  SetMotorSpeed(setPoint, setPoint);
+  //SetMotorPower(setPoint, setPoint);
 }
 
 //Call this to setup drive system
