aa7b42eea837d538d2c172a048d9bdf567ac3e8c
balintmaci <balintmaci@gmail.com>
2017-03-07 16:30:33 +0100

    comment

--- main/myFunctions.h
+++ main/myFunctions.h
@@ -4,6 +4,7 @@ float mapfloat(float x, float in_min, float in_max, float out_min, float out_max
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+//Function to return sign of any type
 template <typename type>
 type sign(type value) { 
  return type((value>0)-(value<0)); 
