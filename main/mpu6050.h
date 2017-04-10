3e66146fc462875257b1e021e62ef7fe8fe16bf8
balintmaci <balintmaci@gmail.com>
2017-04-10 10:55:52 +0200

    mpu library

---
+++ main/mpu6050.h
@@ -0,0 +1,22 @@
+const int MPU_addr = 0x68;
+volatile int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+void Setupmpu6050() {
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+}
+void Readmpu6050() {
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 14, true);
+  AcX = Wire.read() << 8 | Wire.read();
+  AcY = Wire.read() << 8 | Wire.read();
+  AcZ = Wire.read() << 8 | Wire.read();
+  Tmp = Wire.read() << 8 | Wire.read();
+  GyX = Wire.read() << 8 | Wire.read();
+  GyY = Wire.read() << 8 | Wire.read();
+  GyZ = Wire.read() << 8 | Wire.read();
+}
